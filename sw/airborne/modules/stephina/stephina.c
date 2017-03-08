43903ccd9c9f4f27726e12f20d53cdd33c8f1af7
mavlabCourse <info@mavlab.info>
2017-03-08 14:25:55 +0100

    new module

---
+++ sw/airborne/modules/stephina/stephina.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) karan
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/stephina/stephina.c"
+ * @author karan
+ * stephan als een meiske
+ */
+
+#include "modules/stephina/stephina.h"
+
+void stephani_init() {
+
+git}
+// void stephi {}
+
+
